cccc0d2ebb801be2751ae7369a540eb33f8cf728
DI-786 <diyaf4600@gmail.com>
2026-06-25 17:15:20 +0300

    added js for reservations

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ window.addEventListener('resize', function() {
         nav.style.display = 'flex';
     }
 });
+function toggleEventType() {
+            const reservationType = document.getElementById('reservation-type').value;
+            const eventTypeSection = document.getElementById('event-type-section');
+            const eventTypeSelect = document.getElementById('event-type');
+            
+            if (reservationType === 'event') {
+                eventTypeSection.style.display = 'block';
+                eventTypeSelect.required = true;
+            } else {
+                eventTypeSection.style.display = 'none';
+                eventTypeSelect.required = false;
+            }
+        }
